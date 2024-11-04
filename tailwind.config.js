fb16a6ae5602f7c7d522e4bb8e2b9f39b7e50110
ebnaayadepu <ebnaepu@gmail.com>
2024-11-04 23:28:40 +0600

    tailwind-css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["*"],
+  theme: {
+    "fontFamily": {
+      'open':['"Open Sans", sans-serif']
+    }
+  },
+  plugins: [],
+}
+
